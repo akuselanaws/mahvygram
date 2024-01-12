748f09f7ff3cbb386a58817a7f853f3df46bd0b8
Anand Babu Kuselan <anandbabu_kuselan@comcast.com>
2024-01-12 13:06:11 -0500

    Sign-up completion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // import React from 'react'
 
 import { Routes, Route } from 'react-router-dom';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages'
+import SigninForm from '@/_auth/forms/SigninForm';
+import SignupForm from '@/_auth/forms/SignupForm';
+import { Home } from '@/_root/pages'
 import './globals.css';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
